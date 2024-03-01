ecd04f8f4a7ace6f90f6ba41d58a32ac402d5f7f
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-03-01 11:04:28 +0530

    added navbar and structure of main section

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.css'
 import Image from 'next/image'
-import Logo from '../../../public/logo.png'
+import Logo from '../../../public/logo1.png'
+import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className={styles.nav}>
@@ -9,10 +10,14 @@ const Navbar = () => {
             <Image
             src={Logo}
             alt='logo'
+            className={styles.logo}
             />
         </div>
         <div className={styles.rightNav}>
-
+            <h4><Link href='/'>FEATURES</Link></h4>
+            <h4><Link href='/'>GAMES</Link></h4>
+            <h4><Link href='/'>ABOUT</Link></h4>
+            <h4><Link href='/'>DOWNLOAD</Link></h4>
         </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 // import Image from "next/image";
-// import styles from "./page.module.css";
+import styles from "../app/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
-    this is main section
-    </>
+    <div className={styles.mainWrapperDiv}>
+      
+      <div className={styles.page1}>
+        <div className={styles.page1ContentDiv}>
+        <div className={styles.page1Left}>
+
+        </div>
+        <div className={styles.page1Right}>
+
+        </div>
+
+      </div>
+      </div>
+      
+    </div>
   );
 }
