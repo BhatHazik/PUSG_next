8034390eb84901ba70d5db2c709119c3eb9e3b28
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-03-02 14:17:33 +0530

    improvement in page1 and added new scroll bar

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,10 @@ import styles from "../app/styles/Home.module.css";
 import Image from "next/image";
 import kelly from "../app/Assets/kelly.png"
 import arrow from "../app/Assets/img14.png"
+import android from "../app/Assets/img3.png"
+import Link from "next/link";
+import web from "./Assets/img15.png"
+import sideArrows from "./Assets/img19.png"
 export default function Home() {
   return (
     <div className={styles.mainWrapperDiv}>
@@ -18,12 +22,20 @@ export default function Home() {
         </div>
         <div className={styles.page1Left2}>
         <div className={styles.downloadButtons}>
-            <div className={styles.androidDownloadButton}>
-
-            </div>
-            <div className={styles.webLoginButton}>
-
-            </div>
+            <Link href='/' className={styles.androidDownloadButton}>
+              <Image src={android}/>
+              <div>
+                <h3>Download</h3>
+                <h2>Android App</h2>
+              </div>
+            </Link>
+            <Link href='/' className={styles.webLoginButton}>
+            <Image src={web}/>
+              <div>
+                <h3>Login on</h3>
+                <h2>Website</h2>
+              </div>
+            </Link>
           </div>
         </div>
 
@@ -36,6 +48,7 @@ export default function Home() {
         </div>
 
       </div>
+      <Image src={sideArrows} className={styles.page1SideArrows}/>
       </div>
       
     </div>
