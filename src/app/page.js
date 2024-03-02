b02cdb5eb8d8048d283b6c4f0a8bdf800a14f4ee
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-03-02 17:03:26 +0530

    added page2 sec1 and aditional fixes

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,16 @@ import android from "../app/Assets/img3.png"
 import Link from "next/link";
 import web from "./Assets/img15.png"
 import sideArrows from "./Assets/img19.png"
+import skill from "./Assets/img7.png"
+import iso from "./Assets/img5.png"
+import star from "./Assets/img4.png"
+import joistick from "./Assets/img11.png"
+import gstCoin from "./Assets/img20.png"
 export default function Home() {
   return (
     <div className={styles.mainWrapperDiv}>
+
+      {/* page1 structure */}
 
       <div className={styles.page1}>
         <div className={styles.page1ContentDiv}>
@@ -49,6 +56,21 @@ export default function Home() {
 
       </div>
       <Image src={sideArrows} className={styles.page1SideArrows}/>
+      </div>
+
+      {/* page2 structure */}
+
+      <div className={styles.page2}>
+        <div className={styles.circlesHolder}>
+          <span><div><h4>4.3</h4><Image src={star} className={styles.page2Image1}/></div><h3>57,894</h3><h3>Reviews</h3></span>
+          <span><div><Image src={iso} className={styles.page2Image2}/></div><h3>ISO</h3><h3>Certified</h3></span>
+          <span><div><Image src={skill} className={styles.page2Image3}/></div><h3>100%</h3><h3>Skill Games</h3></span>
+          <span><div><Image src={joistick} className={styles.page2Image4}/></div><h3>100%</h3><h3>Gamer Friendly</h3></span>
+          <span><div><Image src={gstCoin} className={styles.page2Image5}/></div><h3>100%</h3><h3>GST and TDS</h3><h3>Refund</h3></span>
+        </div>
+        <div className={styles.FeaturesHolder}>
+          
+        </div>
       </div>
       
     </div>
